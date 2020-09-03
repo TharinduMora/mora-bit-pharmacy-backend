92b4b50cf8fb14eb842ab18b9583b02b626b8815
Tharindu <tharindu199411@gmail.com>
2020-09-03 12:28:49 +0530

    Update shop.routes.js

--- app/routes/shop.routes.js
+++ app/routes/shop.routes.js
@@ -3,6 +3,17 @@ const express = require('express');
 const router = express.Router();
 const commonFunctions = require("../shared/common.functions");
 
+const requireJsonContent = (funcId) => {
+    return (req, res, next) => {
+        commonFunctions.authValidator(req, res, funcId, next);
+        // if (req.headers['content-type'] !== 'application/json') {
+        //     res.status(400).send('Server requires application/json')
+        // } else {
+        //     next()
+        // }
+    }
+}
+
 router.use(function (req, res, next) {
     console.log('Time:', Date.now());
     req.admin = {
@@ -11,10 +22,10 @@ router.use(function (req, res, next) {
     // res.send("Error");
     next()
 });
-
-router.get('/', function (req, res, next) {
-    commonFunctions.authValidator(req, res, 3, next);
-}, shops.findAll);
+router.get('/', requireJsonContent(3), shops.findAll);
+// router.get('/', function (req, res, next) {
+//     commonFunctions.authValidator(req, res, 3, next);
+// }, shops.findAll);
 router.get('/:shopId', shops.findOne);
 router.post('/', function (req, res, next) {
     commonFunctions.authValidator(req, res, 1, next);

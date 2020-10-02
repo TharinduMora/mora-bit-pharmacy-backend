fdf5f9128a418ef5c39b3f77202f3d0d9c1b8283
Tharindu Jayasinghe <tharindu199411@gmail.com>
2020-10-02 23:50:31 +0530

    master data

--- server.js
+++ server.js
@@ -54,6 +54,7 @@ app.use("/", (req, res, next) => {
 app.use("/shop", require("./app/routes/shop.routes.js"));
 app.use("/admin", require("./app/routes/admin.routes.js"));
 app.use("/requests", require("./app/routes/api.request.routes"));
+app.use("/masterData", require("./app/routes/master.data.routes"));
 
 app.listen(appConfig.SERVER.PORT, () => {
     logger.info(`${appConfig.APP_NAME} started on port ${appConfig.SERVER.PORT}`);

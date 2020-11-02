3c23030966f041d57bd9bd1511d193d2318c22cf
Tharindu <tharindu199411@gmail.com>
2020-11-02 18:57:52 +0530

    Update product.controller.js

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -13,67 +13,74 @@ const logger = require("../shared/logger/logger.module")("product.controller.js"
 const dbTransaction = require("../shared/database/db.transaction.chain");
 
 exports.create = async (req, res) => {
-    // Validate the Request and reformat to api format
-    if (!commonFunctions.validateRequestBody(req.body, ProductApiRequest.CREATE_API, true, res))
-        return;
 
-    if (!commonFunctions.isValidToProcess(req, res, req.body.shopId))
-        return;
+    try {
+        // Validate the Request and reformat to api format
+        if (!commonFunctions.validateRequestBody(req.body, ProductApiRequest.CREATE_API, true, res))
+            return;
 
-    let product = new Product({
-        stockAvailable: req.body.stockAvailable,
-        name: req.body.name,
-        description: req.body.description,
-        image: req.body.image
-    });
-    product.shopId = req.body.shopId;
-    product.status = mainConfig.SYSTEM_STATUS.PENDING;
+        if (!commonFunctions.isValidToProcess(req, res, req.body.shopId))
+            return;
 
-    const txn = dbTransaction.getTransaction();
+        let product = new Product({
+            stockAvailable: req.body.stockAvailable,
+            name: req.body.name,
+            description: req.body.description,
+            image: req.body.image
+        });
+        product.shopId = req.body.shopId;
+        product.status = mainConfig.SYSTEM_STATUS.PENDING;
 
-    const productResponse = await txn.persist(Product, product);
+        const txn = dbTransaction.getTransaction();
 
-    if (DbResponses.isSqlErrorResponse(productResponse.status)) {
-        logger.error(productResponse.data.sqlMessage || "Internal Server Error!")
-        res.status(500).send(ResponseFactory.getErrorResponse({message: productResponse.data.sqlMessage || "Internal Server Error!"}));
-        return;
+        const productResponse = await txn.persist(Product, product);
+
+        if (DbResponses.isSqlErrorResponse(productResponse.status)) {
+            logger.error(productResponse.data.sqlMessage || "Internal Server Error!")
+            res.status(500).send(ResponseFactory.getErrorResponse({message: productResponse.data.sqlMessage || "Internal Server Error!"}));
+            return;
+        }
+
+        product = productResponse.data;
+
+        let productInventory = new ProductInventory({
+            price: req.body.price
+        });
+
+        if (product && product.id > 0) {
+            productInventory.productId = product.id;
+            productInventory.shopId = product.shopId;
+        } else {
+            logger.error("Product Id is not valid");
+            res.status(500).send(ResponseFactory.getErrorResponse({message: "Internal Server Error!"}));
+            txn.rollback();
+            return;
+        }
+
+        const productInventoryResponse = await txn.persist(ProductInventory, productInventory);
+
+        if (DbResponses.isSqlErrorResponse(productInventoryResponse.status)) {
+            logger.error(productResponse.data.sqlMessage || "Internal Server Error!")
+            res.status(500).send(ResponseFactory.getErrorResponse({message: productInventoryResponse.data.sqlMessage || "Internal Server Error!"}));
+            return;
+        }
+
+        const queryResponse = await txn.commit();
+
+        if (DbResponses.isRollback(queryResponse.status)) {
+            logger.error(queryResponse.data.sqlMessage || "Internal Server Error!")
+            res.status(500).send(ResponseFactory.getErrorResponse({message: queryResponse.data.sqlMessage || "Internal Server Error!"}));
+            return;
+        }
+
+        logger.info("Product Created. Id: " + product.id);
+        res.send(ResponseFactory.getSuccessResponse({
+            data: {...product, price: productInventory.price},
+            message: "Product Created"
+        }));
+    } catch (e) {
+        logger.error(e);
+        res.status(500).send(ResponseFactory.getErrorResponse({message: 'Internal Server Error'}));
     }
 
-    product = productResponse.data;
-
-    let productInventory = new ProductInventory({
-        price: req.body.price
-    });
-
-    if (product && product.id > 0) {
-        productInventory.productId = product.id;
-        productInventory.shopId = product.shopId;
-    } else {
-        logger.error("Product Id is not valid");
-        res.status(500).send(ResponseFactory.getErrorResponse({message: "Internal Server Error!"}));
-        txn.rollback();
-        return;
-    }
-
-    const productInventoryResponse = await txn.persist(ProductInventory, productInventory);
-
-    if (DbResponses.isSqlErrorResponse(productInventoryResponse.status)) {
-        logger.error(productResponse.data.sqlMessage || "Internal Server Error!")
-        res.status(500).send(ResponseFactory.getErrorResponse({message: productInventoryResponse.data.sqlMessage || "Internal Server Error!"}));
-        return;
-    }
-
-    const queryResponse = await txn.commit();
-
-    if (DbResponses.isRollback(queryResponse.status)) {
-        logger.error(queryResponse.data.sqlMessage || "Internal Server Error!")
-        res.status(500).send(ResponseFactory.getErrorResponse({message: queryResponse.data.sqlMessage || "Internal Server Error!"}));
-        return;
-    }
-
-    logger.info("Product Created. Id: " + product.id);
-    res.send(ResponseFactory.getSuccessResponse({
-        data: {...product, price: productInventory.price},
-        message: "Product Created"
-    }));
 };
